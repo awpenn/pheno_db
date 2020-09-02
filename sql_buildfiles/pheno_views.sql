7161d5b3da2e8c97d8b7bf49229ecb2b582a1504
Andrew Wilk <andrewwilk@client0321-hnt.vpn.private.upenn.edu>
2020-09-02 11:39:43 -0400

    r in jupyterhub install

--- sql_buildfiles/pheno_views.sql
+++ sql_buildfiles/pheno_views.sql
@@ -13,26 +13,46 @@ WHERE data_version
     )
 )
 
+CREATE OR REPLACE VIEW get_all_ds
+AS
+SELECT 
+    subject_id,
+    CAST(_data::json->>'sex' as INT) as sex,
+    _data::json->>'prevad' as prevad,
+    _data::json->>'incad' as incad,
+    CAST(_data::json->>'age' as INT) as age,
+    CAST(_data::json->>'age_baseline' as INT) as age_baseline,
+    _data::json->>'apoe' as apoe,
+    _data::json->>'autopsy' as autopsy,
+    _data::json->>'braak' as braak,
+    CAST(_data::json->>'race' as INT) as race,
+    _data::json->>'ethnicity' as ethnicity,
+    CAST(_data::json->>'selection' as INT) as selection,
+    CAST(_data::json->>'ad' as INT) as ad,
+    _data::json->>'comments' as comments,
+    CAST(_data::json->>'data_version' as INT) as data_version
+FROM ds_subjects_phenotypes
+
 
 CREATE OR REPLACE VIEW get_current_ds
 AS
 SELECT 
    subject_id, 
 
-_data::json->'sex' as sex,
-_data::json->'prevad' as prevAD,
-_data::json->'incad' as incAD,
-_data::json->'age' as age,
-_data::json->'age_baseline' as age_baseline,
-_data::json->'apoe' as apoe,
-_data::json->'autopsy' as autopsy,
-_data::json->'braak' as braak,
-_data::json->'race' as race,
-_data::json->'ethnicity' as ethnicity,
-_data::json->'selection' as selection,
-_data::json->'ad' as AD,
-_data::json->'comments' as comments,
-_data::json->'data_version' as data_version
+CAST(_data::json->>'sex' as INT) as sex,
+_data::json->>'prevad' as prevad,
+_data::json->>'incad' as incad,
+CAST(_data::json->>'age' as INT) as age,
+CAST(_data::json->>'age_baseline' as INT) as age_baseline,
+_data::json->>'apoe' as apoe,
+_data::json->>'autopsy' as autopsy,
+_data::json->>'braak' as braak,
+CAST(_data::json->>'race' as INT) as race,
+_data::json->>'ethnicity' as ethnicity,
+CAST(_data::json->>'selection' as INT) as selection,
+CAST(_data::json->>'ad' as INT) as ad,
+_data::json->>'comments' as comments,
+CAST(_data::json->>'data_version' as INT) as data_version
 FROM ds_subjects_phenotypes
 WHERE CAST(_data->>'data_version' as INT) IN (
         select id from data_versions where version_date = (select max(version_date) from data_versions)
