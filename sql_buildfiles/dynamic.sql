26d2ae3fb55c2ebb032768674ef7c0abd2b88de0
Andrew Wilk <andrewwilk@client0645vag.vpn.private.upenn.edu>
2020-09-15 16:27:22 -0400

    added function and view for filtering only unpublished update entries

--- sql_buildfiles/dynamic.sql
+++ sql_buildfiles/dynamic.sql
@@ -93,3 +93,16 @@ CREATE OR REPLACE FUNCTION get_newest_dyno() RETURNS TABLE
 			end loop;
 	END;
 $$ LANGUAGE plpgsql;
+
+/*Only return entries that for subject_id with highest data_version number AND are unpublished*/
+CREATE OR REPLACE FUNCTION get_updates_dyno() RETURNS TABLE
+(id int, subject_id char varying, _data jsonb, published boolean) AS $$
+	declare
+		i RECORD;
+	BEGIN
+		for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes GROUP BY ds_subjects_phenotypes.subject_id)
+			loop
+				return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.published from ds_subjects_phenotypes where ds_subjects_phenotypes.subject_id = i.subject_id and ds_subjects_phenotypes._data->>'data_version' = i.dv AND ds_subjects_phenotypes.published = FALSE;
+			end loop;
+	END;
+$$ LANGUAGE plpgsql;
--- sql_buildfiles/pheno_views.sql
+++ sql_buildfiles/pheno_views.sql
@@ -126,3 +126,26 @@ published
 
 FROM get_newest_dyno()
 ORDER BY subject_id;
+
+CREATE OR REPLACE VIEW get_unpublished_updates_dynamic
+AS
+SELECT 
+subject_id, 
+CAST(_data::json->>'sex' as INT) as sex,
+_data::json->>'prevad' as prevad,
+_data::json->>'incad' as incad,
+CAST(_data::json->>'age' as INT) as age,
+CAST(_data::json->>'age_baseline' as INT) as age_baseline,
+_data::json->>'apoe' as apoe,
+_data::json->>'autopsy' as autopsy,
+_data::json->>'braak' as braak,
+CAST(_data::json->>'race' as INT) as race,
+_data::json->>'ethnicity' as ethnicity,
+CAST(_data::json->>'selection' as INT) as selection,
+CAST(_data::json->>'ad' as INT) as ad,
+_data::json->>'comments' as comments,
+CAST(_data::json->>'data_version' as INT) as data_version,
+published
+
+FROM get_updates_dyno()
+ORDER BY subject_id;
