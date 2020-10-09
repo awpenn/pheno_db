1d6b9d17e6ac8abee3556cf9e35c3e7bddbfe546
Andrew Wilk <andrewwilk@client0645vag.vpn.private.upenn.edu>
2020-10-09 10:12:24 -0400

    adding subject_type filtering to dyno functions

--- sql_buildfiles/dynamic.sql
+++ sql_buildfiles/dynamic.sql
@@ -1,18 +1,18 @@
 /*get the entry with highest published vers_no for a particular subject_id*/
-CREATE OR REPLACE FUNCTION get_current_published_dyno() RETURNS TABLE
+CREATE OR REPLACE FUNCTION get_current_published_cc_dyno() RETURNS TABLE
 (id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
 	declare
 		i RECORD;
 	BEGIN
-		for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes where ds_subjects_phenotypes.published = TRUE GROUP BY ds_subjects_phenotypes.subject_id)
+		for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes WHERE ds_subjects_phenotypes.published = TRUE GROUP BY ds_subjects_phenotypes.subject_id)
 			loop
-				return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published from ds_subjects_phenotypes where ds_subjects_phenotypes.subject_id = i.subject_id and ds_subjects_phenotypes._data->>'data_version' = i.dv;
+				return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published FROM ds_subjects_phenotypes WHERE ds_subjects_phenotypes.subject_id = i.subject_id AND ds_subjects_phenotypes.subject_type = 'case/control' AND ds_subjects_phenotypes._data->>'data_version' = i.dv;
 			end loop;
 	END;
 $$ LANGUAGE plpgsql;
 
 /*get the entry with highest vers_no for a particular subject_id, regardless of publication status*/
-CREATE OR REPLACE FUNCTION get_newest_dyno() RETURNS TABLE
+CREATE OR REPLACE FUNCTION get_newest_cc_dyno() RETURNS TABLE
 (id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
 	declare
 		i RECORD;
@@ -25,7 +25,7 @@ CREATE OR REPLACE FUNCTION get_newest_dyno() RETURNS TABLE
 $$ LANGUAGE plpgsql;
 
 /*Only return entries that for subject_id with highest data_version number AND are unpublished*/
-CREATE OR REPLACE FUNCTION get_updates_dyno() RETURNS TABLE
+CREATE OR REPLACE FUNCTION get_updates_cc_dyno() RETURNS TABLE
 (id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
 	declare
 		i RECORD;
@@ -36,3 +36,56 @@ CREATE OR REPLACE FUNCTION get_updates_dyno() RETURNS TABLE
 			end loop;
 	END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_current_published_fam_dyno() RETURNS TABLE
+(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
+	declare
+		i RECORD;
+	BEGIN
+		for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes where ds_subjects_phenotypes.published = TRUE GROUP BY ds_subjects_phenotypes.subject_id)
+			loop
+				return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published from ds_subjects_phenotypes where ds_subjects_phenotypes.subject_id = i.subject_id and ds_subjects_phenotypes._data->>'data_version' = i.dv;
+			end loop;
+	END;
+$$ LANGUAGE plpgsql;
+
+/*get the entry with highest vers_no for a particular subject_id, regardless of publication status*/
+CREATE OR REPLACE FUNCTION get_newest_fam_dyno() RETURNS TABLE
+(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
+	declare
+		i RECORD;
+	BEGIN
+		for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes GROUP BY ds_subjects_phenotypes.subject_id)
+			loop
+				return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published from ds_subjects_phenotypes where ds_subjects_phenotypes.subject_id = i.subject_id and ds_subjects_phenotypes._data->>'data_version' = i.dv;
+			end loop;
+	END;
+$$ LANGUAGE plpgsql;
+
+/*Only return entries that for subject_id with highest data_version number AND are unpublished*/
+CREATE OR REPLACE FUNCTION get_updates_fam_dyno() RETURNS TABLE
+(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
+	declare
+		i RECORD;
+	BEGIN
+		for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes GROUP BY ds_subjects_phenotypes.subject_id)
+			loop
+				return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published from ds_subjects_phenotypes where ds_subjects_phenotypes.subject_id = i.subject_id and ds_subjects_phenotypes._data->>'data_version' = i.dv AND ds_subjects_phenotypes.published = FALSE;
+			end loop;
+	END;
+$$ LANGUAGE plpgsql;
+
+
+--10/8 note-- 
+--need to make dynois fior cc and fam separate to account for same subject_id with c/c and fam records, ala
+-- CREATE OR REPLACE FUNCTION get_current_published_dyno() RETURNS TABLE
+-- (id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
+-- 	declare
+-- 		i RECORD;
+-- 	BEGIN
+-- 		for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes WHERE ds_subjects_phenotypes.subject_type = 'case/control' AND ds_subjects_phenotypes.published = TRUE GROUP BY ds_subjects_phenotypes.subject_id)
+-- 			loop
+-- 				return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published from ds_subjects_phenotypes where ds_subjects_phenotypes.subject_id = i.subject_id and ds_subjects_phenotypes._data->>'data_version' = i.dv;
+-- 			end loop;
+-- 	END;
+-- $$ LANGUAGE plpgsql;
