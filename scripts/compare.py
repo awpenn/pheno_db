0aaf24bd2358eb3c5e76a74ce5349d1754b53ad2
Andrew Wilk <andrewwilk@client0336vag.vpn.private.upenn.edu>
2020-10-20 16:06:19 -0400

    produces sorted dataframe from database calls

--- scripts/compare.py
+++ scripts/compare.py
@@ -6,42 +6,67 @@ import csv
 from dotenv import load_dotenv
 import os
 import json
+import pandas as pd
 
 import calendar
 import time
-
+ 
 from pheno_utils import *
 
 new_records = []
 success_id_log = []
 error_log = {}
 
-LOADFILE = ''
-compare_family_data = False
+compare_family_data = ''
 
 def main():
     """main conductor function for the script."""
-    global compare_family_data
+    user_input_subject_type = get_subject_type()
     
-    compare_family_data = get_subject_type()
-    comparison_dict_and_headers = create_comparison_dict()
+    if user_input_subject_type == 'case/control':
+        views_based_on_subject_type = 'get_current_cc', 'get_unpublished_updates_cc'
+    if user_input_subject_type == 'family':
+        views_based_on_subject_type = 'get_current_fam', 'get_unpublished_updates_fam'
+
+    comparison_dict_and_headers = create_comparison_dict( views_based_on_subject_type )
 
     build_comparison_table( comparison_dict_and_headers )
 
-def create_comparison_dict():
-    """takes no args and creates comparison dict and list of headers"""
-    global compare_family_data
+def create_comparison_dict( views_based_on_subject_type ):
+    """takes views based on subject type as arg, and creates comparison dict and list of headers"""
     compare_dict = {}
     headers_list = []
+    current_view, update_view = views_based_on_subject_type
 
-    if compare_family_data:
-        data = database_connection("SELECT * FROM get_unpublished_updates_fam LEFT JOIN get_current_fam ON get_unpublished_updates_fam.subject_id = get_current_fam.subject_id")
-        headers = database_connection("SELECT table_name, ordinal_position, column_name FROM information_schema.columns WHERE table_name in('get_unpublished_updates_cc' ,'get_current_fam');" )
-    else:
-        data = database_connection("SELECT * FROM get_unpublished_updates_cc LEFT JOIN get_current_cc ON get_unpublished_updates_cc.subject_id = get_current_cc.subject_id")
-        headers = database_connection("SELECT ordinal_position, column_name, table_name FROM information_schema.columns WHERE table_name in('get_unpublished_updates_cc' ,'get_current_cc');")
+    data = database_connection(f"SELECT * FROM {update_view} LEFT JOIN {current_view} ON {update_view}.subject_id = {current_view}.subject_id")
+    # headers = database_connection(f"SELECT table_name, ordinal_position, column_name FROM information_schema.columns WHERE table_name in('{update_view}' ,'{current_view}');" )
+    header_data = database_connection(f"SELECT column_name FROM information_schema.columns WHERE table_name in('{update_view}' ,'{current_view}');" )
     
-    unique_headers_len = int( len( headers )/2 )
+    headers_unpacked = [''.join(header) for header in header_data] 
+    unique_headers_len = int( len( headers_unpacked )/2 )
+    headers_cleaned = []
+    headers_sorted = []
+
+    for index, header in enumerate( headers_unpacked ):
+        """takes headers, gives the 'current' ones a 'baseline_' appendage"""
+        if index >= unique_headers_len:
+            headers_cleaned.append( f"baseline_{header}" )
+        else:
+            headers_cleaned.append( header )
+
+    for index, header in enumerate( headers_cleaned ):
+        """orders cleaned headers so like columns are next to eachother (may discard)"""
+        if index < unique_headers_len:
+            headers_sorted.append( header )
+            headers_sorted.append( headers_cleaned[ headers_cleaned.index( f"baseline_{header}" ) ] )
+    
+    #this unsorted_df is same as the merged tables in JM code
+    unsorted_df = pd.DataFrame(data, columns=[headers_cleaned])
+    #this is same frame but with like columns next to eachother
+    sorted_df = unsorted_df[headers_sorted]
+
+    breakpoint()
+    
 
     for p_value in data:
         subject_id = p_value[ 0 ]
