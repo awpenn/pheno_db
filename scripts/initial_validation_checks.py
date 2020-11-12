24e2bdaef29a6f1acad47d21c0c462f8bf339d6e
Andrew Wilk <andrewwilk@client0369vag.vpn.private.upenn.edu>
2020-11-12 11:20:16 -0500

    some building

--- scripts/initial_validation_checks.py
+++ scripts/initial_validation_checks.py
@@ -15,5 +15,10 @@ from pheno_utils import *
 # def a():
 def validation_checks_overlord( subject_type, data_dict ):
     """takes the subject_type and data_dict from pheno handling scripts, calls series of validation checks and returns ???"""
-    ## for testing, will read from dict.json file instead of normal function workflow, remove after
+    dictionary = get_dict_data( subject_type )
+    breakpoint()
+
+if __name__ == '__main__':
+     ## for testing, will read from dict.json file instead of normal function workflow, remove after
     data_dict = get_test_json_from_file( 'data.json' ) ##this is case/control data from `starting_data_for_test` file
+    validation_checks_overlord( 'case/control', data_dict )
