c3e4db1039bdba2c0830ce14ef85e406d9b2d673
Andrew Wilk <andrewwilk@client0369vag.vpn.private.upenn.edu>
2020-11-16 11:33:43 -0500

    error handling works

--- scripts/initial_validation_checks.py
+++ scripts/initial_validation_checks.py
@@ -6,6 +6,7 @@ import csv
 from dotenv import load_dotenv
 import os
 import json
+import datetime
 
 import pandas as pd
 
@@ -24,8 +25,19 @@ def main():
     dictionary = get_dict_data( dict_name )
 
     reviewed_dict = run_checks( data_dict, dictionary )
-    df = pd.read_json( json.dumps( reviewed_dict ) ).transpose()
-    df.to_csv(f"./comparison_files/tada.txt",sep="\t",index=True,na_rep="NA")
+    for key, value in reviewed_dict.items():
+
+        if 'data_errors' in value.keys():
+            df = pd.read_json( json.dumps( reviewed_dict ) ).transpose()
+            create_tsv( df, user_input_subject_type )
+            print(f"One or more data errors found in { LOADFILE }. A tsv with error flags will be generated.")
+            ## Found an error, generated the tsv and now will exit. 
+            sys.exit()
+        else:
+            # print(f'Found no errors { key }')
+            continue
+    
+    print(f"No data errors found in { LOADFILE }.")
 
 def create_data_dict( LOADFILE, subject_type ):
     """takes loadfile, subject_type as args, returns dict of json data keyed by subject id of data to be valcheck"""
@@ -72,21 +84,21 @@ def run_checks( data_dict, dictionary ):
         if 'age' in phenotype:
             try:
                 if int( pheno_value ) not in range( 121 ):
-                    print()
-                    return [ pheno_value, f"{pheno_value} in not valid" ]
+                    return [ pheno_value, f"'{pheno_value}' is NOT valid for { phenotype }" ]
                 else:
                     return pheno_value
 
             except:
                 if pheno_value != 'NA':
-                    return [ pheno_value, f"{pheno_value} in not valid" ]
+                    return [ pheno_value, f"'{pheno_value}' is NOT valid for { phenotype }" ]
+        
         else:
             if len( dict_values ) > 1:
 
                 if str(pheno_value) in dict_values:
                     return pheno_value
                 else:
-                    return [ pheno_value, f'{ phenotype } value { pheno_value } is NOT valid.' ]
+                    return [ pheno_value, f"'{ pheno_value }' is NOT valid for { phenotype }." ]
             else:
                 return pheno_value
 
@@ -95,21 +107,40 @@ def run_checks( data_dict, dictionary ):
 
     for key, value in data_dict.items():
         reviewed_subject_object = {}
+        error_list = []
         for phenotype, pheno_value in value.items():
             ## gets values_dict in dictionary by the current phenotype in current subject entry in data_dict.
             try:
                 dict_values = dictionary.data_values[ dictionary.variable == f'{ phenotype }' ].values[ 0 ]
-                reviewed_subject_object[ phenotype ] = check_data_value( key, dict_values, phenotype, pheno_value )
-            except Exception as e:
-                print(phenotype, e)
-                print(f"{ phenotype } does not appear in the dictionary.")
-                reviewed_subject_object[ phenotype ] = pheno_value
 
-        review_dict[ key ] = reviewed_subject_object
+            except Exception as e:
+                # print(f"{ phenotype } does not appear in the dictionary { key }.")
+                reviewed_subject_object[ phenotype ] = pheno_value
+                continue
+
+            checked_phenotype_value = check_data_value( key, dict_values, phenotype, pheno_value )
+
+            if isinstance( checked_phenotype_value, list ):
+                p_value, error_msg = checked_phenotype_value
+                reviewed_subject_object [ phenotype ] = p_value
+                error_list.append( error_msg )
+
+            else:
+                reviewed_subject_object[ phenotype ] = checked_phenotype_value
+
+        ## if errors, take the compiled errors list, add to subject's data object, concatenating list items, separated by semi-col
+        if len( error_list ) > 0:
+            reviewed_subject_object[ 'data_errors' ] = '; '.join( error_list )
+
+        review_dict[ key.split('_') [ 0 ] ] = reviewed_subject_object
 
     return review_dict
-    ## this is almost exactly same as the function in load_phenotypes, so consider generalizing original and calling across files          
 
+def create_tsv( dataframe, subject_type ):
+    """takes the compiled dataframe and subject-type (to formulate filename) and creates a TSV."""
+    datestamp = datetime.date.today()
+    corrected_subject_type = subject_type.replace( "/", "+" )
 
+    dataframe.to_csv(f"./comparison_files/{ corrected_subject_type }-validation_errors-{ datestamp }.txt",sep="\t",index=True)
 if __name__ == '__main__':
     main()  
