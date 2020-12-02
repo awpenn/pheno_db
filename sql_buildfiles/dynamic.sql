c4b71e16bf0fc95f59b07a5aefe94918a61d7628
Andrew Wilk <andrewwilk@client0794-hnt.vpn.private.upenn.edu>
2020-12-02 15:49:50 -0500

    added cdb dynamic funcs

--- sql_buildfiles/dynamic.sql
+++ sql_buildfiles/dynamic.sql
@@ -78,7 +78,7 @@
 		END;
 	$$ LANGUAGE plpgsql;
 
-/*adniy*/
+/*adni*/
 	/*get the entry with highest published vers_no for a particular cc subject_id*/
 	CREATE OR REPLACE FUNCTION get_current_published_adni_dyno() RETURNS TABLE
 	(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
@@ -107,6 +107,46 @@
 
 	/*Only return entries that for cc subject_id with highest data_version number AND are unpublished*/
 	CREATE OR REPLACE FUNCTION get_updates_adni_dyno() RETURNS TABLE
+	(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
+		declare
+			i RECORD;
+		BEGIN
+			for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes GROUP BY ds_subjects_phenotypes.subject_id)
+				loop
+					return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published from ds_subjects_phenotypes WHERE ds_subjects_phenotypes.subject_id = i.subject_id AND ds_subjects_phenotypes._data->>'data_version' = i.dv AND ds_subjects_phenotypes.subject_type = 'ADNI' AND ds_subjects_phenotypes.published = FALSE;
+				end loop;
+		END;
+	$$ LANGUAGE plpgsql;
+
+/*psp/cdb*/
+	/*get the entry with highest published vers_no for a particular cc subject_id*/
+	CREATE OR REPLACE FUNCTION get_current_published_psp_cdb_dyno() RETURNS TABLE
+	(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
+		declare
+			i RECORD;
+		BEGIN
+			for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv from ds_subjects_phenotypes WHERE ds_subjects_phenotypes.published = TRUE GROUP BY ds_subjects_phenotypes.subject_id)
+				loop
+					return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published FROM ds_subjects_phenotypes WHERE ds_subjects_phenotypes.subject_id = i.subject_id AND ds_subjects_phenotypes.subject_type = 'ADNI' AND ds_subjects_phenotypes._data->>'data_version' = i.dv;
+				end loop;
+		END;
+	$$ LANGUAGE plpgsql;
+
+	/*get the entry with highest vers_no for a particular cc subject_id, regardless of publication status*/
+	CREATE OR REPLACE FUNCTION get_newest_psp_cdb_dyno() RETURNS TABLE
+	(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
+		declare
+			i RECORD;
+		BEGIN
+			for i in (select distinct ds_subjects_phenotypes.subject_id, max(ds_subjects_phenotypes._data->>'data_version') as dv FROM ds_subjects_phenotypes GROUP BY ds_subjects_phenotypes.subject_id)
+				loop
+					return query select ds_subjects_phenotypes.id, ds_subjects_phenotypes.subject_id, ds_subjects_phenotypes._data, ds_subjects_phenotypes.subject_type, ds_subjects_phenotypes.published FROM ds_subjects_phenotypes WHERE ds_subjects_phenotypes.subject_id = i.subject_id AND ds_subjects_phenotypes.subject_type = 'ADNI' AND ds_subjects_phenotypes._data->>'data_version' = i.dv;
+				end loop;
+		END;
+	$$ LANGUAGE plpgsql;
+
+	/*Only return entries that for cc subject_id with highest data_version number AND are unpublished*/
+	CREATE OR REPLACE FUNCTION get_updates_psp_cdb_dyno() RETURNS TABLE
 	(id int, subject_id char varying, _data jsonb, subject_type subject_type, published boolean) AS $$
 		declare
 			i RECORD;
--- sql_buildfiles/pheno_table_build.sql
+++ sql_buildfiles/pheno_table_build.sql
@@ -1,5 +1,5 @@
 /*enum lists*/
-CREATE TYPE "public"."subject_type" AS ENUM('case/control', 'family', 'ADNI', 'other');   
+CREATE TYPE "public"."subject_type" AS ENUM('case/control', 'family', 'ADNI', 'CDB/PSP', 'other');   
 /* Table definitions for phenotype database*/
 
 CREATE TABLE IF NOT EXISTS "data_versions" 
