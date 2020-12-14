118fae97b46dfed85301d0a777a602582e7586db
Andrew Wilk <andrewwilk@client0274vag.vpn.private.upenn.edu>
2020-12-14 16:35:38 -0500

    function in initial check makes sure that variables in loadfile match those in appropriate dictionary

--- scripts/initial_validation_checks.py
+++ scripts/initial_validation_checks.py
@@ -13,28 +13,43 @@ import pandas as pd
 from pheno_utils import *
 
 def main():
-    user_input_subject_type = get_subject_type()
-    LOADFILE = get_filename()
+    # user_input_subject_type = get_subject_type()
+    # LOADFILE = get_filename()
+
+    ## 12/13 debug
+    user_input_subject_type = 'case/control'
+    # LOADFILE = 'starting_data_for_test.csv'
+    LOADFILE = 'adni.csv'
 
     data_dict = create_data_dict( LOADFILE, user_input_subject_type )
     dict_name = database_connection(f"SELECT dictionary_name FROM env_var_by_subject_type WHERE subject_type = '{ user_input_subject_type }'")[ 0 ][ 0 ]
     dictionary = get_dict_data( dict_name )
 
-    reviewed_dict = run_checks( data_dict, dictionary )
-    for key, value in reviewed_dict.items():
+    variables_match_dictionary, msg = check_loadfile_variables_match_dictionary( data_dict, dictionary, user_input_subject_type )
+    
+    if not variables_match_dictionary:
+        print( msg )
+        sys.exit()
+    else:
+        print( msg )
 
-        if 'data_errors' in value.keys():
-            ## have to flip the dataframe columns/rows with .transpose()
-            df = pd.read_json( json.dumps( reviewed_dict ) ).transpose()
-            create_tsv( df, user_input_subject_type )
-            print(f"One or more data errors found in { LOADFILE }. A tsv with error flags will be generated.")
-            ## Found an error, generated the tsv and now will exit. 
-            sys.exit()
-        else:
-            # print(f'Found no errors { key }')
-            continue
 
-    print(f"No data errors found in { LOADFILE }.")
+        breakpoint()
+        reviewed_dict = run_checks( data_dict, dictionary )
+        for key, value in reviewed_dict.items():
+
+            if 'data_errors' in value.keys():
+                ## have to flip the dataframe columns/rows with .transpose()
+                df = pd.read_json( json.dumps( reviewed_dict ) ).transpose()
+                create_tsv( df, user_input_subject_type )
+                print(f"One or more data errors found in { LOADFILE }. A tsv with error flags will be generated.")
+                ## Found an error, generated the tsv and now will exit. 
+                sys.exit()
+            else:
+                # print(f'Found no errors { key }')
+                continue
+
+        print(f"No data errors found in { LOADFILE }.")
 
 def create_data_dict( LOADFILE, subject_type ):
     """takes loadfile, subject_type as args, returns dict of json data keyed by subject id of data to be valcheck"""
